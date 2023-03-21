0ffe663f8236a55bb36dab282b3dc2d99e49558f
Martin Janssen <martinjanssen01@gmail.com>
2023-03-21 11:36:39 +0100

    Add score endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,14 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from classes import Data
+from classes.data import Data
+from classes.scorer import Scorer
 
 
 app = FastAPI()
 
 data = Data()
+scorer = Scorer(data.df)
 
 @app.get("/")
 def read_root():
@@ -18,15 +20,16 @@ def read_root():
 def get_all_algorithms():
     return ORJSONResponse(data.get_all_records())
 
+@app.get('/algorithms/score', response_class=ORJSONResponse)
+def get_overal_score_all_algorithms():
+    
+    return ORJSONResponse(scorer.get_overall_score().toJSON())
+
 @app.get('/algorithms/{algo_id}', response_class=ORJSONResponse)
 def get_one_algorithm_by_id(algo_id:int):
     return ORJSONResponse(data.get_record_by_id(algo_id))
 
-@app.get('algorithms/score')
-def get_overal_score_all_algorithms():
-    return {}
-
-@app.get('algorithms/{algo_id}/score')
+@app.get('/algorithms/{algo_id}/score')
 def get_score_for_algorithm(algo_id:int):
     return {}
 
