9e61117d3de985587f0245981e8aa10d65ecdf24
Martin Janssen <martinjanssen01@gmail.com>
2023-03-21 11:36:25 +0100

    Add Scorer object

--- backend/classes/scorer.py
+++ backend/classes/scorer.py
@@ -1,8 +1,13 @@
 import pandas as pd
+from .score import Score
+from .score_value import ScoreValue
 
 class Scorer:
     
     def __init__(self, data: pd.DataFrame) -> None:
         self.data = data
+
+    def get_overall_score(self):
+        return ScoreValue(Score(self.data))
     
     
